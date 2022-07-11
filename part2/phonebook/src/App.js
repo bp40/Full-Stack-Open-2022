5fb58ca5914b6c90a723df13badcad61d053c60f
bp40 <60870525+bp40@users.noreply.github.com>
2022-07-11 20:37:04 +0700

    ex 2.18 - save state in json db

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,48 +1,18 @@
 import { useEffect, useState } from "react";
-import Filter from "./Filter";
-import PersonForm from "./PersonForm";
-import Persons from "./Persons";
-import axios from "axios"
+import Filter from "./components/Filter";
+import NewPersonForm from "./components/NewPersonForm";
+import PersonsList from "./components/PersonsList";
+import personsService from "./services/personsService";
 
 const App = () => {
 	const [persons, setPersons] = useState([]);
-	const [newName, setNewName] = useState("");
-	const [newNumber, setNewNumber] = useState("");
 	const [searchTerm, setNewSearchTerm] = useState("");
 
 	useEffect(() => {
-		axios.get("http://localhost:3001/persons").then(res => {
-			setPersons(res.data)
-		})
-	}, [])
-
-	const handleNameChange = (event) => {
-		event.preventDefault();
-		setNewName(event.target.value);
-	};
-
-	const handlePersonSubmit = (event) => {
-		event.preventDefault();
-
-		const newPersonObject = {
-			name: newName,
-			number: newNumber,
-		};
-
-		for (let i = 0; i < persons.length; i++) {
-			if (persons[i].name === newName) {
-				alert(`${newName} is already added to phonebook`);
-				return;
-			}
-		}
-
-		setPersons(persons.concat(newPersonObject));
-	};
-
-	const handleNumberChange = (event) => {
-		event.preventDefault();
-		setNewNumber(event.target.value);
-	};
+		personsService.getAll().then((data) => {
+			setPersons(data);
+		});
+	}, []);
 
 	const handleSearchChange = (event) => {
 		event.preventDefault();
@@ -52,20 +22,16 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange}/>
+			<Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
 
-			<h2>add a new</h2>
-			<PersonForm
-				newName={newName}
-				newNumber={newNumber}
-				handleNameChange={handleNameChange}
-				handleNumberChange={handleNumberChange}
-				handlePersonSubmit={handlePersonSubmit}
-			/>
+			<NewPersonForm persons={persons} setPersons={setPersons} />
 
 			<h2>Numbers</h2>
-      <Persons persons={persons} searchTerm={searchTerm}/>
-			
+			<PersonsList
+				persons={persons}
+				searchTerm={searchTerm}
+				setPersons={setPersons}
+			/>
 		</div>
 	);
 };
---
+++ part2/phonebook/src/components/NewPersonForm.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+import personsService from "../services/personsService";
+
+const NewPersonForm = ({ persons, setPersons }) => {
+	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
+
+	const checkNameExist = (name) => {
+		for (let i = 0; i < persons.length; i++) {
+			if (persons[i].name === name) {
+				return persons[i].id;
+			}
+		}
+		return -1;
+	};
+
+	const handleNameChange = (event) => {
+		event.preventDefault();
+		setNewName(event.target.value);
+	};
+
+	const handlePersonSubmit = (event) => {
+		event.preventDefault();
+
+		const newPersonObject = {
+			name: newName,
+			number: newNumber,
+			id: persons.length + 1,
+		};
+
+		const indexIfPersonExist = checkNameExist(newPersonObject.name);
+
+		if (indexIfPersonExist !== -1) {
+			if (
+				window.confirm(
+					newPersonObject.name + " is already added. Replace with new info?"
+				)
+			) {
+				newPersonObject.id = indexIfPersonExist + 1;
+				personsService.update(indexIfPersonExist, newPersonObject).then(() => {
+					personsService.getAll().then((data) => {
+						setPersons(data);
+					});
+				});
+			}
+		} else {
+			setPersons(persons.concat(newPersonObject));
+			personsService.create(newPersonObject);
+		}
+	};
+
+	const handleNumberChange = (event) => {
+		event.preventDefault();
+		setNewNumber(event.target.value);
+	};
+
+	return (
+		<>
+			<h2>add a new</h2>
+			<form onSubmit={handlePersonSubmit}>
+				<div>
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					number: <input value={newNumber} onChange={handleNumberChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+		</>
+	);
+};
+
+export default NewPersonForm;
