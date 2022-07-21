82814e58a0d8343f45f4e759a688d32185f9ba7b
bp40 <60870525+bp40@users.noreply.github.com>
2022-07-21 23:36:24 +0700

    change baseUrl for backend

--- part2/phonebook/src/services/personsService.jsx
+++ part2/phonebook/src/services/personsService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://shielded-badlands-32937.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
