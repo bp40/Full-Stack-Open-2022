1a3fa630926c07c738f5341988c00ecc9a24298b
bp40 <60870525+bp40@users.noreply.github.com>
2023-03-24 15:23:29 +0700

    feat: add logging middleware

--- part4/app.js
+++ part4/app.js
@@ -1,4 +1,3 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,18 +5,30 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const {info, error} = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
-
-
+const middleware = require('./utils/middleware')
+const logger = require("./utils/logger");
 
 if (process.argv.length < 3) {
     console.log('Please provide the password as an argument: node index.js <password>');
     process.exit(1);
 }
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("connected to mongo")
+    })
+    .catch(() => {
+        logger.error("error connecting to mongo")
+    })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
