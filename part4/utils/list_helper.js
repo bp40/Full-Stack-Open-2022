614eb49903aa4af541573509267739f8e196c452
bp40 <60870525+bp40@users.noreply.github.com>
2023-03-25 19:53:02 +0700

    test: add favorite blog test

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -16,7 +16,31 @@ const totalLikes = (blogs) => {
     return sumLikes
 }
 
+const favoriteBlog = (blogs) => {
+
+    if (blogs.length === 1) {
+        return blogs[0]
+    }
+
+    let highestLikes = 0
+    let favoriteIndex = 0
+    for (let i = 0; i < blogs.length; i++) {
+        if (highestLikes < blogs[i].likes) {
+            highestLikes = blogs[i].likes
+            favoriteIndex = i
+        }
+    }
+
+    return {
+        title: blogs[favoriteIndex].title,
+        author: blogs[favoriteIndex].author,
+        likes: highestLikes
+    }
+
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
